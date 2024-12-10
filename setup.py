57e71bf02921baf81cd6fa74e8b38cfbef8b0830
周小羽 <xiaoyuzhou@bytedance.com>
2024-12-10 11:30:56 +0800

    release 0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependencies = [
 
 setup(
     name="causalmatch",
-    version='0.0.2',
+    version='0.0.3',
     author="Xiaoyu Zhou",
     author_email="xiaoyuzhou@bytedance.com",
     url='https://github.com/bytedance/CausalMatch',
